d8d914955e82c600a92f6367d8b04ea5d77c87c6
didar <deddarnawzad@gmail.com>
2024-03-10 20:54:57 +0300

    init

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="bg-red-900 p-5 flex justify-between">
+    <div class="border items-center flex px-2">brand-log</div>
+    <RouterLink
+      to="/signup"
+      class="border px-4 py-2 bg-black rounded-md text-white hover:bg-gray-800 active:bg-slate-900"
+    >
+      SIGN UP
+    </RouterLink>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
