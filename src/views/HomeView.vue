7467c92d6ab0df592ba3ef4ae8d6ad84d3856012
didar <deddarnawzad@gmail.com>
2024-03-12 21:49:26 +0300

    define routerView to show diffrent route in HomeView component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <NavBar />
+  <RouterView />
 </template>
 
 <script setup>
 import NavBar from '@/components/NavBar.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <style lang="scss" scoped></style>
