2899e0d3e61ae8475fd26215a685d698932d6078
didar <deddarnawzad@gmail.com>
2024-03-12 21:58:46 +0300

    create JobCard component to display jobs

---
+++ src/components/JobCard.vue
@@ -0,0 +1,23 @@
+<template>
+  
+    <div class="">
+      <img
+        class="object-cover m-auto size-14 rounded-xl"
+        src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+        alt=""
+      />
+    </div>
+    <div class="grow">
+      <p class="text-2xl">forntend developer</p>
+      <span class="text-sm">sulaymany</span>
+    </div>
+    <div class="flex flex-col justify-between">
+      <p class="p-1 text-center text-white bg-black rounded-lg">full-time</p>
+      <p class="text-sm">1/1/2024</p>
+    </div>
+ 
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
