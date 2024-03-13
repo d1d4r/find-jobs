7589835fd9e4acae67e4c07f6a12d221038369ea
didar <deddarnawzad@gmail.com>
2024-03-13 05:17:57 +0300

    make component dynamic render and clickable  change route

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <div class="flex gap-2 p-3 border cursor-pointer hover:border-black" @click="toDetails(post.id)">
     <div class="">
       <img
         class="object-cover m-auto size-14 rounded-xl"
@@ -8,16 +8,30 @@
       />
     </div>
     <div class="grow">
-      <p class="text-2xl">forntend developer</p>
-      <span class="text-sm">sulaymany</span>
+      <p class="text-2xl">{{ post.jobTitle }}</p>
+      <span class="text-sm">{{ post.location }}</span>
     </div>
     <div class="flex flex-col justify-between">
-      <p class="p-1 text-center text-white bg-black rounded-lg">full-time</p>
-      <p class="text-sm">1/1/2024</p>
+      <p class="p-1 text-center text-white bg-black rounded-lg">{{ post.employmentType }}</p>
+      <p class="text-sm">{{ post.applicationDeadline }}</p>
     </div>
- 
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+
+defineProps({
+  post: {
+    type: Object,
+    required: true
+  }
+})
+
+const router = useRouter()
+const toDetails = (id) => {
+  router.push({ path: `jobs/${id}` })
+}
+</script>
 
 <style lang="scss" scoped></style>
