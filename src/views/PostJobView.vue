88bc818e0f2224beda15feb375304cc8f82dd4b2
didar <deddarnawzad@gmail.com>
2024-03-14 23:15:46 +0300

    set requried and placeholder to all inputs

--- src/views/PostJobView.vue
+++ src/views/PostJobView.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container p-4 mx-auto">
     <h1 class="mb-4 text-2xl font-bold">Job Posting Form</h1>
-    <form @submit.prevent="handleSubmit">
+    <form @submit.prevent="submitForm">
       <div class="mb-4">
         <label for="jobTitle" class="block mb-2 font-bold">Job Title:</label>
         <input
+          required
+          placeholder="write title or position job"
           v-model="formData.jobTitle"
           type="text"
           id="jobTitle"
@@ -14,6 +16,8 @@
       <div class="mb-4">
         <label for="description" class="block mb-2 font-bold">Description:</label>
         <textarea
+          required
+          placeholder="write simple description about position or job"
           v-model="formData.description"
           id="description"
           rows="5"
@@ -24,26 +28,36 @@
         <div>
           <label for="requirements" class="block mb-2 font-bold">Requirements:</label>
           <textarea
+            required
+            placeholder="write requirements for qualicaed canditate"
             v-model="formData.requirements"
             id="requirements"
             rows="3"
             class="w-full px-3 py-2 leading-tight border border-black rounded appearance-none"
           ></textarea>
         </div>
-        <div>
-          <label for="location" class="block mb-2 font-bold">Location:</label>
-          <input
+        <div class="mb-4">
+          <label for="experienceLevel" class="block mb-2 font-bold">location:</label>
+          <select
+            required
             v-model="formData.location"
-            type="text"
-            id="location"
+            id="experienceLevel"
             class="w-full px-3 py-2 leading-tight border border-black rounded appearance-none"
-          />
+          >
+            <option value="" selected disabled hidden>location</option>
+            <option value="Silemani">Silemani</option>
+            <option value="Duhok">Duhok</option>
+            <option value="Kerkuk">Kerkuk</option>
+            <option value="Erbil">Erbil</option>
+          </select>
         </div>
       </div>
       <div class="grid grid-cols-1 gap-4 mb-4 md:grid-cols-2">
         <div>
           <label for="basedSalary" class="block mb-2 font-bold">Base Salary:</label>
           <input
+            placeholder="write base salary $"
+            required
             v-model="formData.basedSalary"
             type="number"
             id="basedSalary"
@@ -53,6 +67,8 @@
         <div>
           <label for="skills" class="block mb-2 font-bold">Skills (comma separated):</label>
           <input
+            placeholder="example js,frontend,api"
+            required
             v-model="formData.skills"
             type="text"
             id="skills"
@@ -63,6 +79,7 @@
       <div class="mb-4">
         <label for="employmentType" class="block mb-2 font-bold">Employment Type:</label>
         <select
+          required
           v-model="formData.employmentType"
           id="employmentType"
           class="w-full px-3 py-2 leading-tight border border-black rounded appearance-none"
@@ -76,6 +93,7 @@
       <div class="mb-4">
         <label for="experienceLevel" class="block mb-2 font-bold">Experience Level:</label>
         <select
+          required
           v-model="formData.experienceLevel"
           id="experienceLevel"
           class="w-full px-3 py-2 leading-tight border border-black rounded appearance-none"
@@ -94,17 +112,19 @@
       <div class="mb-4">
         <label for="applicationDeadline" class="block mb-2 font-bold">Application Deadline:</label>
         <input
+          required
           v-model="formData.applicationDeadline"
           type="date"
           id="applicationDeadline"
           class="w-full px-3 py-2 leading-tight border border-black rounded"
-          required
         />
         <!-- <div v-if="errors.applicationDeadline" class="mt-1 text-xs text-red-500">{{ errors.applicationDeadline }}</div> -->
       </div>
       <div class="mb-4">
         <label for="tags" class="block mb-2 font-bold">Tags/Keywords (comma separated):</label>
         <input
+          placeholder="react,js,c# ..."
+          required
           v-model="formData.tags"
           type="text"
           id="tags"
@@ -114,7 +134,6 @@
       <div class="flex items-center justify-center gap-2 p-5">
         <button
           :disabled="isLoading"
-          @click="submitForm"
           class="px-4 py-2 text-white bg-black border rounded-md hover:bg-gray-800 active:bg-slate-900 disabled:bg-slate-400"
           type="submit"
         >
