b5c56d7244da42a01c1145c707cb397e26bfb3b5
didar <deddarnawzad@gmail.com>
2024-03-15 23:24:04 +0300

    write footer for home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
   <NavBar />
   <RouterView />
+  <footer class="h-20 p-2 bg-black border">
+    <div class="text-center text-white">
+      <p>
+        developed by
+        <a
+          href="https://github.com/d1d4r"
+          target="_blank"
+          class="p-1 text-black underline bg-white rounded-md"
+        >
+          d1d4r
+        </a>
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script setup>
