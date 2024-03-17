2065103bda8cd3c0dab9aecb7f92749960e9b068
didar <deddarnawzad@gmail.com>
2024-03-17 23:55:34 +0300

    write two functions getUserById, updateUser to manage user and get specific user

--- src/services/firestore/useUsers.js
+++ src/services/firestore/useUsers.js
@@ -1,6 +1,15 @@
-import { addDoc, collection, doc, setDoc } from 'firebase/firestore'
+import { addDoc, collection, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
 import { db } from '@/config/firebase'
+import { reactive } from 'vue'
+import { updateProfile } from 'firebase/auth'
+import { auth } from '@/config/firebase'
 export const useUser = () => {
+  const userState = reactive({
+    users: [],
+    user: {},
+    error: null,
+    loading: false
+  })
   const createUser = async (data, id) => {
     try {
       const usersCollection = doc(db, 'Users', id)
@@ -9,6 +18,37 @@ export const useUser = () => {
       console.log('🚀 ~ createUser ~ error:', error)
     }
   }
+  const getUserById = async (id) => {
+    try {
+      const user = await getDoc(doc(collection(db, 'Users'), id))
+      if (user.exists()) {
+        userState.user = user.data()
+      } else {
+        console.log('No such document!')
+        userState.users = {}
+      }
+    } catch (error) {
+      console.log('🚀 ~ getUserById ~ error:', error)
+    }
+  }
 
-  return { createUser }
+  const updateUser = async (id, data) => {
+    try {
+      const { displayName, bio } = data
+      const userDocRef = doc(db, 'Users', id)
+      await updateProfile(auth.currentUser, { displayName })
+
+      await updateDoc(userDocRef, {
+        displayName,
+        bio
+      })
+
+    } catch (error) {
+      console.log('🚀 ~ updateUser ~ error:', error)
+    }
+  }
+
+ 
+
+  return { userState, createUser, getUserById, updateUser }
 }
