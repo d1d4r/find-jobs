eda347b20c29fe0cb6f41a86d6ff3cded9888222
didar <deddarnawzad@gmail.com>
2024-03-20 02:08:13 +0300

    write watch to forece view re-render after filters change

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -6,7 +6,7 @@
       </h2>
     </div>
     <div class="flex justify-center mt-[-2rem] bg-white m-8 rounded-lg border p-2">
-      <FilterJobs :getPosts="getPostsService" />
+      <FilterJobs :getPostsService="getPostsService" />
     </div>
     <div class="flex flex-col w-full min-h-screen p-5 m-auto space-y-1 border md:w-1/2">
       <span class="self-start pb-5 text-2xl">job listing</span>
@@ -27,7 +27,7 @@
     </div>
     <div class="flex justify-center my-5 space-x-2 text-xl">
       <!-- <button :disabled="isFirstPage" @click="prev">prev</button> -->
-      <button
+      <!-- <button
         class="p-2"
         :class="{ ' text-white bg-black rounded-md': currentPage === item }"
         v-for="item in pageCount"
@@ -41,7 +41,7 @@
         "
       >
         {{ item }}
-      </button>
+      </button> -->
       <!-- <button :disabled="isLastPage" @click="next">next</button> -->
     </div>
   </div>
@@ -50,26 +50,28 @@
 <script setup>
 import JobCard from '@/components/JobCard.vue'
 import FilterJobs from '@/components/FilterJobs.vue'
-import { onMounted, computed, ref, onUnmounted } from 'vue'
+import { onMounted, computed, ref, onUnmounted, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useOffsetPagination } from '@vueuse/core'
 import { getPostsService, countJobs } from '@/services/firestore/jobService'
 import { reactive } from 'vue'
 import { onSnapshot } from 'firebase/firestore'
+import { useFilterJobsStore } from '@/stores/useFilterJobsStore'
 
+const { filters } = useFilterJobsStore()
 const route = useRoute()
 const router = useRouter()
 const placesLength = computed(() => postState.count)
 const page = ref(route.query.page)
 const pageSize = ref(4)
 
-const { currentPage, pageCount, isFirstPage, isLastPage, prev, next } = useOffsetPagination({
-  total: 4,
-  page,
-  pageSize,
-  onPageChange: fetchData,
-  onPageSizeChange: fetchData
-})
+// const { currentPage, pageCount, isFirstPage, isLastPage, prev, next } = useOffsetPagination({
+//   total: 4,
+//   page,
+//   pageSize,
+//   onPageChange: fetchData,
+//   onPageSizeChange: fetchData
+// })
 
 const itemTop = (item) => {
   window.scrollTo(0, 0)
@@ -116,7 +118,11 @@ onMounted(async () => {
   postState.count = count
   fetchJobs()
 
-  await fetchData({ currentPage: page.value, currentPageSize: pageSize.value })
+  //await fetchData({ currentPage: page.value, currentPageSize: pageSize.value })
+})
+
+watch(filters, () => {
+  fetchJobs()
 })
 
 onUnmounted(() => {
