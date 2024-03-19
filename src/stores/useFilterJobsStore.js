10db52a8dbd80a41769011a62e419aeb4f239134
didar <deddarnawzad@gmail.com>
2024-03-20 02:12:44 +0300

    chnage store name and remove comments

--- src/stores/useFilterJobsStore.js
+++ src/stores/useFilterJobsStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { reactive } from 'vue'
 
-export const useFilterJobsStore = defineStore('filter-store', () => {
+export const useFilterJobsStore = defineStore('filters', () => {
   const filters = reactive({
     location: 'All-Location',
     experienceLevel: 'Experience-Level',
@@ -17,45 +17,3 @@ export const useFilterJobsStore = defineStore('filter-store', () => {
     setFilter
   }
 })
-// export const useFilterJobsStore = defineStore('filter-store', () => {
-//   const location = ref('All-Location')
-//   const setLocation = (payload) => {
-//     location.value = payload
-//   }
-
-//   const experince = ref('Experince-Level')
-//   const setExperince = (payload) => {
-//     experince.value = payload
-//   }
-
-//   const employment = ref('Employment-Type')
-//   const setEmployment = (payload) => {
-//     employment.value = payload
-//   }
-
-//   return {
-//     setLocation,
-//     setExperince,
-//     setEmployment,
-//     location,
-//     experince,
-//     employment
-//   }
-// })
-
-// export const useFilterJobsStore = defineStore('filter-store', () => {
-//   const filters = {
-//     location: ref('All-Location'),
-//     experience: ref('Experience-Level'),
-//     employment: ref('Employment-Type')
-//   }
-
-//   const setFilter = (filterName, payload) => {
-//     filters[filterName].value = payload
-//   }
-
-//   return {
-//     filters,
-//     setFilter
-//   }
-// })
