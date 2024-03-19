547c4fce9b8ba97d68b0d513f53b20b4277b4d35
didar <deddarnawzad@gmail.com>
2024-03-20 02:08:53 +0300

    refactor component and display real candidate

--- src/views/CandidatesView.vue
+++ src/views/CandidatesView.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="grid grid-cols-1 gap-5 m-2 sm:grid-cols-2 md:grid-cols-4 place-items-center">
-      <div
-        class="w-full mx-auto overflow-hidden bg-white border rounded-lg"
-        v-for="i in 9"
-        :key="i"
-      >
-        <img class="object-cover object-center w-full h-56" :src="user.imageUrl" alt="User Image" />
-        <div class="p-4">
-          <h2 class="text-2xl font-semibold text-gray-800">{{ user.name }}</h2>
-          <p class="text-sm font-medium text-gray-500">{{ user.email }}</p>
-        </div>
+    <div
+      class="w-full mx-auto overflow-hidden bg-black border rounded-lg"
+      v-for="candidate in state.candidates"
+      :key="candidate.id"
+    >
+      <img
+        class="object-cover object-center w-full h-56"
+        src="https://via.placeholder.com/150"
+        alt="User Image"
+      />
+      <div class="p-4">
+        <h2 class="text-2xl font-semibold text-white">{{ candidate.displayName }}</h2>
+        <p class="text-sm font-medium text-white">{{ candidate.bio }}</p>
       </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-const user = {
-  name: 'John Doe',
-  email: 'johndoe@example.com',
-  imageUrl: 'https://via.placeholder.com/150' // Replace with actual image URL
-}
+import { getAllUsers } from '@/services/firestore/userService'
+import { onMounted, reactive } from 'vue'
+
+const state = reactive({
+  candidates: []
+})
+onMounted(async () => {
+  const users = await getAllUsers()
+  state.candidates = users
+})
 </script>
 
 <style lang="scss" scoped></style>
