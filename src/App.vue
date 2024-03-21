f59ade37ff5b8f26584901dc520e32c1074ff2e0
didar <deddarnawzad@gmail.com>
2024-03-22 00:30:09 +0300

    add navbar and footer component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
-
+import FooterSrction from '@/components/FooterSection.vue'
 </script>
 
 <template>
   <NavBar />
   <RouterView />
+  <FooterSrction />
 </template>
 
 <style scoped></style>
