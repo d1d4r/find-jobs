70e4d468f901a1ba9164bd4289c47dc7f11fe2f8
didar <deddarnawzad@gmail.com>
2024-03-22 00:30:48 +0300

    write footersection component

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="h-20 p-2 bg-black border">
+    <div class="flex items-center justify-center w-full h-full text-white border">
+      <p>
+        developed by
+        <a
+          href="https://github.com/d1d4r"
+          target="_blank"
+          class="p-1 text-black underline bg-white rounded-md"
+        >
+          d1d4r
+        </a>
+      </p>
+    </div>
+  </footer>
+</template>
