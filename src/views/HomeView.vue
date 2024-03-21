7dcbf7d17bcaa948d484066d068e9c581c648d6d
didar <deddarnawzad@gmail.com>
2024-03-22 00:27:44 +0300

    add hersection component and remove routerview and footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,9 @@
 <template>
-  <!-- <NavBar /> -->
-  
-  <RouterView />
-  <footer class="h-20 p-2 bg-black border">
-    <div class="flex items-center justify-center w-full h-full text-white border">
-      <p>
-        developed by
-        <a
-          href="https://github.com/d1d4r"
-          target="_blank"
-          class="p-1 text-black underline bg-white rounded-md"
-        >
-          d1d4r
-        </a>
-      </p>
-    </div>
-  </footer>
+  <HeroSection />
 </template>
 
 <script setup>
-import NavBar from '@/components/NavBar.vue'
-import { RouterView } from 'vue-router'
+import HeroSection from '@/components/HeroSection.vue'
 </script>
 
-<style lang="scss" scoped></style>
+
