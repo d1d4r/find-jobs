a16ba76c3573370210a409bc54dd927ece50126e
didar <deddarnawzad@gmail.com>
2024-03-26 22:44:56 +0300

    add footer to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <HeroSection />
   <ShowCase />
+  <FooterSection/>
 </template>
 
 <script setup>
+import FooterSection from '@/components/FooterSection.vue';
 import HeroSection from '@/components/HeroSection.vue'
 import ShowCase from '@/components/ShowCase.vue'
 </script>
